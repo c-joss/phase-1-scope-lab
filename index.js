7e6987555e89ddf8ab699f43225dc12337dc90cc
Courtney Macgregor <cjoss90@hotmail.com>
2024-09-22 17:17:57 +1000

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "john";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "scott";
+    return leastFavoriteCustomer;
+}
+
